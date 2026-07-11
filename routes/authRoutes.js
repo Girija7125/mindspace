01d3a43e7c15f6b3e2087ce5f628929ecd3c5098
Girija Sankar Giri <girija.giri2k@gmail.com>
2026-07-11 20:13:55 +0530

    Add rate limiting to login endpoint to prevent brute-force attacks

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express= require('express');
 const router=express.Router();
+const {loginLimiter}=require('../middleware/rateLimiter')
+
 
 
 const {registerUser,logInUser}= require('../controllers/authController');
 
 router.post('/register',registerUser);
-router.post('/login',logInUser)
+router.post('/login',loginLimiter,logInUser);
 
 module.exports = router;
